dac0934b6056aadecf6139fd1a8bd6b504ae06ff
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-03-20 00:04:05 +0100

    changing serializer again

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,7 +1,15 @@
 class V1::UserSerializer < ActiveModel::Serializer
   attributes(:id, :email, :username, :tmp_profile_link, :steam_profile_link)
   attribute :birth_date, key: :age
-  has_one :user_joined_vtc, serializer: V1::UserJoinedVtcSerializer
+  has_one :user_joined_vtc
+
+  def user_joined_vtc
+    if object.user_joined_vtcs.length == 1
+      Vtc.find_by(id: object.user_joined_vtc.id)
+    else
+      nil
+    end
+  end
 
   def birth_date
     object.get_user_age
