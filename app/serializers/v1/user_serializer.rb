5566cf3ec215d90be66acb0d555e01cfdbbeef04
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-03-19 22:47:12 +0100

    still fixing

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -4,8 +4,11 @@ class V1::UserSerializer < ActiveModel::Serializer
   has_one :user_joined_vtc
 
   def user_joined_vtc
-    if object.user_joined_vtc.vtc_id.present?
+    case object.reflect_on_association(:user_joined_vtc)
+    when ActiveRecord::Reflection::HasOneReflection
       Vtc.find_by(id: object.user_joined_vtc.vtc_id)
+    else
+      nil
     end
   end
 
