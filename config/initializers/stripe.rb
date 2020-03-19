73b9bf4a2e0a107efc1bf2ec2d80d9b083f36560
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-03-19 14:09:55 +0100

    testing prod

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,18 +3,16 @@ StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET'] # e.g. whsec_...
 
 class RecordCharges
   def call(event)
-    # Look up the user in the database
     charge = event.data.object
-    user = User.find_by_stripe_id(charge.customer)
+    user = User.find_by(stripe_id: charge.customer)
     user.charges.create(
         stripe_id: charge.id,
-        amount: charge.amount_paid,
+        amount: charge.amount,
         card_last4: user.card_last4,
         card_exp_month: user.card_expiration_month,
         card_exp_year: user.card_expiration_year,
         card_brand: user.card_brand
     )
-    # Record a charge in our database
   end
 end
 
