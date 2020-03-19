d173794c486ccacd000b8fbb197c0a9634d99ec2
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-03-19 22:04:17 +0100

    user serializer

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -3,6 +3,10 @@ class V1::UserSerializer < ActiveModel::Serializer
   attribute :birth_date, key: :age
   has_one :user_joined_vtc
 
+  def user_joined_vtc
+    Vtc.find_by(id: object.user_joined_vtc.vtc_id)
+  end
+
   def birth_date
     object.get_user_age
   end
