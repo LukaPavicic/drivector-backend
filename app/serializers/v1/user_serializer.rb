750b7459e5b3bbcb84c6b34dce2a16ca9e4e4c7c
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-03-19 23:59:19 +0100

    serializer fix

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,7 +1,7 @@
 class V1::UserSerializer < ActiveModel::Serializer
   attributes(:id, :email, :username, :tmp_profile_link, :steam_profile_link)
   attribute :birth_date, key: :age
-  has_one :user_joined_vtc, serializer: V1::VtcSerializer
+  has_one :user_joined_vtc, serializer: V1::UserJoinedVtcSerializer
 
   def birth_date
     object.get_user_age
